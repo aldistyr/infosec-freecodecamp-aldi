a5baee78adc0fb589b49daebdb3527bd154fa38a
unknown <aldisetyar@gmail.com>
2025-07-21 23:21:06 +0700

    add all

--- myApp.js
+++ myApp.js
@@ -1,11 +1,44 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+const timeInSeconds = 90*24*60*60;
 
 app.use(helmet.hidePoweredBy());
-
-
-
-
+app.use(
+  helmet.frameguard({
+    action: "deny",
+  })
+);
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(
+  helmet.hsts({
+    maxAge: timeInSeconds,
+    force: true
+  })
+);
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({
+  directives : {
+    defaultSrc : ["'self'"],
+    scriptSrc : ["'self'", "trusted-cdn.com"],
+  },
+})
+);
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}));
 
 
 
@@ -49,7 +82,6 @@ app.use(helmet.hidePoweredBy());
 
 module.exports = app;
 const api = require('./server.js');
-const helmet = require('helmet');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
