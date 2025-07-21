a39946a4d01ea96bea67d4d56837194bc990e2fa
unknown <aldisetyar@gmail.com>
2025-07-21 21:07:48 +0700

    add hidePoweredBy

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-
+app.use(helmet.hidePoweredBy());
 
 
 
@@ -49,6 +49,7 @@ const app = express();
 
 module.exports = app;
 const api = require('./server.js');
+const helmet = require('helmet');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
